948b6471447fec6b312fa23fa8129f5583fef865
Aaron Rueth <Aaron Rueth>
2016-11-06 18:12:32 -0800

    Added debugger setup

--- __main__.py
+++ __main__.py
@@ -1,8 +1,13 @@
+import cloudshell.helpers.scripts.cloudshell_dev_helpers as dev_helpers
+
+dev_helpers.attach_to_cloudshell()
+
 from sandbox_scripts.environment.setup.setup_script import EnvironmentSetup
 
 
 def main():
     EnvironmentSetup().execute()
+
 
 if __name__ == "__main__":
     main()
